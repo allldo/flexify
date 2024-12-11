b5c0202fc9e166729aebaad29e61aa771737d3f7
Allldo <dotan2.maks@mail.ru>
2024-12-11 20:36:24 +0500

    superuser

--- cabinet/managers.py
+++ cabinet/managers.py
@@ -8,3 +8,14 @@ class UserManager(BaseUserManager):
         user = self.model(phone_number=phone_number, **extra_fields)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        if not phone_number:
+            raise ValueError("The phone number must be provided")
+        user = self.model(phone_number=phone_number)
+        user.password = user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
